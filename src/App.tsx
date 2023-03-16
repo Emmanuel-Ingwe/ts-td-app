20498d7fa21f6f499b7b4842e4e1d8459b326dfe
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-03-16 03:46:46 +0100

    input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-	return <div className='App'>todo</div>;
+	return (
+		<div className='App'>
+			<div className='header'>
+				<div className='inputContainer'>
+					<input type='text' placeholder='Task...' />
+					<input type='number' placeholder='DeadLine (in Days)...' />
+				</div>
+				<button>Add Task</button>
+			</div>
+			<div className='todoList'></div>
+		</div>
+	);
 }
 
 export default App;
