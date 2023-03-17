bd7797e0fd38f6a740e471f3529f77d92bfb517a
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-03-17 07:11:43 +0100

    complete--task

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,16 @@ const App: FC = () => {
 	const addTask = (): void => {
 		const newTask = { taskName: task, deadline: deadline };
 		setTodoList([...todoList, newTask]);
+		setTask("");
+		setDeadline(0);
+	};
+
+	const completeTask = (taskNameToDelete: string): void => {
+		setTodoList(
+			todoList.filter((task) => {
+				return task.taskName !== taskNameToDelete;
+			})
+		);
 	};
 
 	return (
@@ -44,7 +54,7 @@ const App: FC = () => {
 			</div>
 			<div className='todoList'>
 				{todoList.map((task: ITask, key: number) => {
-					return <TodoTask key={key} task={task} />;
+					return <TodoTask key={key} task={task} completeTask={completeTask} />;
 				})}
 			</div>
 		</div>
